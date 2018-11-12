f4d0cdb23e505f1ff0becf3d36afea9860358b00
Yamil Asusta <hello@yamilasusta.com>
2018-11-12 10:18:40 -0500

    Initial commit

---
+++ install-jenkins/install.sh
@@ -0,0 +1,8 @@
+# Depending on your system you might need to use sudo
+apt-get update
+apt-get install openjdk-8-jdk wget gnupg
+sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9B7D32F2D50582E6
+apt-get update
+apt-get install jenkins
+service jenkins start
---
+++ install-nodejs/install.sh
@@ -0,0 +1,3 @@
+apt-get install curl
+curl -sL https://deb.nodesource.com/setup_11.x | bash -
+apt-get install -y nodejs
